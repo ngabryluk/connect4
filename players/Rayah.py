324ea16ebf43c4268f3d46a2079118b45959b562
ngabryluk <ncgabryluk@gmail.com>
2023-12-05 23:48:10 -0500

    Possibly traverses the states correctly
    Just needs a utility function to be figured out

--- players/Rayah.py
+++ players/Rayah.py
@@ -23,21 +23,29 @@ def get_computer_move(board, which_player):
     """
 
     ai_player_num = which_player
+    move, _ = minimax(board, ai_player_num, ai_player_num, 10)
+    return move + 1
 
 
-def minimax(board, which_player, ai, alpha=-math.inf, beta=math.inf):
+def minimax(board, which_player, ai, depth, alpha=-math.inf, beta=math.inf):
     """Apply minimax with alpha-beta pruning to a node in the tree for the given player."""
 
-    is_gameover, winner = utils.is_gameover(board)
     # Check if we have reached a terminal node
+    is_gameover, winner = utils.is_gameover(board)
     if is_gameover and winner == ai:
         return _, 100
     
     # If we have reached the max depth, calculate the utility of the board
+    # if depth = 0:
+    #     return _, calculate_utility
     # We need to come up with a heuristic to calculate the utility
+    # Possible things to look for in the board
+    #   - Opponent 3-in-a-rows (high priority)
+    #   - Number of 3-in-a-rows
+    #   - Number of 2-in-a-rows
 
     valid_moves = utils.get_valid_moves(board)
-
+    
     # Search for the max player
     if which_player == ai:
         value = -math.inf
@@ -46,7 +54,17 @@ def minimax(board, which_player, ai, alpha=-math.inf, beta=math.inf):
             alpha = max(alpha, value)
             if alpha >= beta:
                 break
+            
+            # Simulate the next move to pass to recursive call
+            rows = utils.get_next_available_rows(board)
+            row = rows[move]
+            board[row][move] = which_player + 1
+
             _, utility = minimax(board, ai - 1, ai, alpha, beta)
+
+            # Bring the board back to original state
+            board[row][move] = 0
+
             if utility > value:
                 value = utility
                 best_move = move
@@ -61,7 +79,17 @@ def minimax(board, which_player, ai, alpha=-math.inf, beta=math.inf):
             beta = min(beta, value)
             if alpha >= beta:
                 break
+
+            # Simulate the next move to pass to recursive call
+            rows = utils.get_next_available_rows(board)
+            row = rows[move]
+            board[row][move] = which_player + 1
+
             _, utility = minimax(board, ai + 1, ai, alpha, beta)
+
+            # Bring the board back to original state
+            board[row][move] = 0
+
             if utility < value:
                 value = utility
                 best_move = move
